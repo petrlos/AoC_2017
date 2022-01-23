c4d46d3abf484ce7b97f3755d0cb28ea16512012
Petr Los <petrlos@gmail.com>
2022-01-23 10:48:26 +0100

    Day 10: new version 1st star

--- 10/10_2.py
+++ 10/10_2.py
@@ -1,24 +1,19 @@
 #Advent of Code 2017: Day 10
-from collections import deque
 def moveRight(data, currentPosition):
     currentPosition = currentPosition % len(data)
     return data[currentPosition:] + data[:currentPosition]
 
 def oneTurn(data, lengths, currentPosition, skipSize):
     for length in lengths:
-        data = moveRight(data, currentPosition)
-        data = list(reversed(data[:length])) + data[length:]
-        data = moveRight(data, - currentPosition)
-        currentPosition += length + skipSize
-        skipSize += 1
-    return data
-
-#MAIN
-
-lengths = [227,169,3,166,246,201,0,47,1,255,2,254,96,3,97,144]
-position = 0
-data = list(range(256))
+        data = moveRight(data, currentPosition) #move right
+        data = list(reversed(data[:length])) + data[length:] #reverse LENGHT elements, addup the rest
+        data = moveRight(data, - currentPosition) #move back left
+        currentPosition += length + skipSize #correct currentPosition by length + skipsize
+        skipSize += 1 #correct skipsize
+    return data, currentPosition, skipSize
 
 #Task1
-result = oneTurn(data, lengths, 0,0)[:2]
+lengths = [227,169,3,166,246,201,0,47,1,255,2,254,96,3,97,144]
+data = list(range(256))
+result, position, skipSize = oneTurn(data, lengths, 0,0)
 print("Task 1:", result[0]*result[1])
