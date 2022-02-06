21c15a27ddc17d0b2f655b352f2a07803ad62fd3
Petr Los <petrlos@gmail.com>
2022-02-06 23:26:46 +0100

    Day 21: Parsing done

--- 21/21.py
+++ 21/21.py
@@ -1,17 +1,25 @@
 #Advent of Code 2017 - Day 21
 import numpy
+from math import sqrt
+from pprint import pprint
 
 def stringToNumpy(string):
-    string = string.replace("/", "").replace("#", "1").replace(".", "0")
-    string = list(map(int, string))
-    sizes = {4: (2,2), 9:(3,3), 16:(4,4)}
-    numpyArray = numpy.zeros(sizes[len(string)])
-    size = sizes[len(string)][0]
+    string = list(map(int, string.replace("/", "").replace("#", "1").replace(".", "0")))
+    size = int(sqrt(len(string)))
+    numpyArray = numpy.zeros((size, size))
     for row in range(size):
         for column in range(size):
             index = size * row + column
             numpyArray[row, column] = string[index]
     return numpyArray
+
+def decodeKey(numpyArray):
+    size = numpyArray.shape[0]
+    reshaped = numpy.reshape(numpyArray, (1, size ** 2))
+    result = 0
+    for index, number in enumerate(range(size ** 2)):
+        result += reshaped[0,number] * (2 ** index)
+    return result
 
 def parseData(lines):
     rulesTwoByTwo = {}
@@ -19,9 +27,17 @@ def parseData(lines):
     for line in lines:
         ruleKey, ruleValue = line.split(" => ")
         ruleKeyNumpy = stringToNumpy(ruleKey)
+        size = ruleKeyNumpy.shape[0]
+        ruleValue = stringToNumpy(ruleValue)
         for flip in range(2):
             for rotate in range(4):
-                ...
+                ruleKey = decodeKey(ruleKeyNumpy)
+                if size == 2:
+                    rulesTwoByTwo[ruleKey] = ruleValue
+                elif size == 3:
+                    rulesThreeByThree[ruleKey] = ruleValue
+                ruleKeyNumpy = numpy.rot90(ruleKeyNumpy)
+            ruleKeyNumpy = numpy.fliplr(ruleKeyNumpy)
     return rulesTwoByTwo, rulesThreeByThree
 
 with open("test.txt") as file:
@@ -29,3 +45,4 @@ with open("test.txt") as file:
 
 
 rulesTwoByTwo, rulesThreeByThree = parseData(lines)
+pprint(rulesTwoByTwo)
